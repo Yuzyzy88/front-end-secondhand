36d45207083d9f18d3f1230f47c3ab6865d8ec52
Yuzyzy88 <ayu.sulistiowati@protonmail.com>
2023-03-25 21:41:07 +0700

    report feature

--- src/components/ListProduct.jsx
+++ src/components/ListProduct.jsx
@@ -5,9 +5,33 @@ import { CardBody, CardSubtitle, CardTitle } from "reactstrap";
 import { Link } from "react-router-dom";
 import "../pages/ProductUpdate.scss";
 import NavbarMenu from "./Navbar";
+import { axiosInstance } from "../axios";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { useState } from "react";
+import Form from 'react-bootstrap/Form';
 
 const ListProduct = ({ setList }) => {
   const { getUserDatabase } = UserAuth();
+  const downloadPDF = () => axiosInstance.get(`download`, {params: {month}}).then(res => {
+    const filepath = res.data.filepath;
+    setTimeout(() => {
+      window.open(filepath, '_blank').focus();
+    }, 1000);
+    setShow(false);
+  });
+
+  const [month, setMonth] = useState("1");
+  const onChange = (event) => {
+    console.log(event);
+    const value = event.target.value;
+    setMonth(value)
+  }
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => { setShow(false) };
+  const handleShow = () => { setShow(true) };
 
   return (
     <div>
@@ -42,6 +66,9 @@ const ListProduct = ({ setList }) => {
                       Edit
                     </button>
                   </Link>
+                  <button type="button" className="btn tombol1" onClick={handleShow}>
+                    Report
+                  </button>
                 </div>
               </div>
             </CardBody>
@@ -53,9 +80,9 @@ const ListProduct = ({ setList }) => {
               className="btn kategori"
               style={{ marginRight: "10px", minWidth: "fit-content" }}
             >
-              <div className=""  onClick={() => setList("produk")}>
-              <FiSearch className="me-2" style={{ fontSize: "20px" }} />
-              Produk
+              <div className="" onClick={() => setList("produk")}>
+                <FiSearch className="me-2" style={{ fontSize: "20px" }} />
+                Produk
               </div>
             </button>
             <button
@@ -64,8 +91,8 @@ const ListProduct = ({ setList }) => {
               style={{ marginRight: "10px", minWidth: "fit-content" }}
             >
               <div className="" onClick={() => setList("diminati")}>
-              <FiSearch className="me-2" style={{ fontSize: "20px" }} />
-              Diminati
+                <FiSearch className="me-2" style={{ fontSize: "20px" }} />
+                Diminati
               </div>
             </button>
             <button
@@ -74,14 +101,45 @@ const ListProduct = ({ setList }) => {
               style={{ marginRight: "10px", minWidth: "fit-content" }}
             >
               <div className="" onClick={() => setList("terjual")}>
-              <FiSearch className="me-2" style={{ fontSize: "20px" }} />
-              Terjual
+                <FiSearch className="me-2" style={{ fontSize: "20px" }} />
+                Terjual
               </div>
             </button>
           </div>
 
         </div>
       </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Dialog>
+          <Modal.Header closeButton>
+            <Modal.Title>Generate Report</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <p>Select Month</p>
+            <Form.Select aria-label="Default select example" onChange={onChange} value={month}>
+              <option value="1">January</option>
+              <option value="2">February</option>
+              <option value="3">March</option>
+              <option value="4">April</option>
+              <option value="5">May</option>
+              <option value="6">June</option>
+              <option value="7">July</option>
+              <option value="8">August</option>
+              <option value="9">September</option>
+              <option value="10">October</option>
+              <option value="11">November</option>
+              <option value="12">December</option>
+            </Form.Select>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button variant="secondary">Close</Button>
+            <Button onClick={downloadPDF} variant="primary">Generate</Button>
+          </Modal.Footer>
+        </Modal.Dialog>
+      </Modal>
     </div>
   );
 };
